6b509220a886d8cf86d145bb045c1d24c7779417
Rowan <Rowanyt2001@gmail.com>
2020-02-09 13:16:19 +0100

    exam started

--- H3_Objects/learn_3.js
+++ H3_Objects/learn_3.js
@@ -1,1 +1,7 @@
-//lesson 3.1
+funnction persoon(voornaam, achternaam, geboortejaar){
+  this.voornaam = voornaam;
+  this.achternaam = achternaam;
+  this.geboortejaar = geboortejaar;
+
+  console.log(this);
+}

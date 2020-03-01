e405b9ae944d540f9a6c7d24b83fdcbd19b37491
Rowan <Rowanyt2001@gmail.com>
2020-03-01 17:24:59 +0100

    chapter3 done

---
+++ H3_Objects/exam/exam.js
@@ -0,0 +1,120 @@
+var data = new Date();
+var D = data.getDate();
+var M = data.getMonth();
+var Y = data.getFullYear();
+
+var inputDay;
+var inputMonth;
+var inputYear;
+
+var interest;
+var amount;
+var doubled;
+var newAmount;
+
+function showData(){
+  getToday();
+  CheckDate();
+  intrests();
+}
+
+function getToday() {
+
+  //see month
+  switch (M) {
+    case 0:
+    document.getElementById("Date").innerHTML = "-January-" + Y;
+    break;
+
+    case 1:
+    document.getElementById("Date").innerHTML = D + "-February-" + Y;
+    break;
+
+    case 2:
+    document.getElementById("Date").innerHTML = D + "-March-" + Y;
+    break;
+
+    case 3:
+    document.getElementById("Date").innerHTML = D + "-April-" + Y;
+    break;
+
+    case 4:
+    document.getElementById("Date").innerHTML = D + "-May-" + Y;
+    break;
+
+    case 5:
+    document.getElementById("Date").innerHTML = D + "-June-" + Y;
+    break;
+
+    case 6:
+    document.getElementById("Date").innerHTML = D + "-July-" + Y;
+    break;
+
+    case 7:
+    document.getElementById("Date").innerHTML = D + "-August-" + Y;
+    break;
+
+    case 8:
+    document.getElementById("Date").innerHTML = D + "-September-" + Y;
+    break;
+
+    case 9:
+    document.getElementById("Date").innerHTML = D + "-October-" + Y;
+    break;
+
+    case 10:
+    document.getElementById("Date").innerHTML = D + "-November-" + Y;
+    break;
+
+    case 11:
+    document.getElementById("Date").innerHTML = D + "-December-" + Y;
+    break;
+  }
+}
+
+function CheckDate(){
+  var input1 = document.getElementById('input1').value;
+  var format = [" ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", "'", ":", '"', "\\", "|", "/", ",", ".", "<", ">", "?"];
+
+  //get split charachter en replace them with -
+  format.forEach(arrayPos => {
+    while(input1.includes(arrayPos)) {
+      input1 = input1.replace(arrayPos, "-");
+    }
+  });
+
+  //array splits
+  var input1Gesplit = input1.split("-");
+
+  inputDay = input1Gesplit[0];
+  inputMonth = input1Gesplit[1];
+  inputYear = input1Gesplit[2];
+
+
+  if(inputDay <= 31 && inputDay >= 1){
+    if (inputMonth <= 12 && inputMonth >= 1 ){
+      if (inputYear <= Y ) {
+        document.getElementById("youDate").innerHTML = input1 ;
+      }else {alert("Not good year");}
+    }else {alert("Not good month");}
+  }else {alert("Not good day");}
+
+}
+
+function intrests(){
+  amount = parseInt(document.getElementById('input2').value);
+  interest = parseInt(document.getElementById('input3').value);
+
+  doubled = amount * 2;
+  var int = (interest / 100) + 1;
+  var i = 0;
+
+  while (amount < doubled) {
+    newAmount = amount * int;
+    amount = newAmount;
+    i++;
+  }
+  document.getElementById("Intrest").innerHTML =  "end of when doubled: " + newAmount.toFixed(2);
+  document.getElementById("yearly").innerHTML =  "Yearly intrest: " + int;
+  document.getElementById("IntYears").innerHTML = "years of intrest: " + i;
+}
---
+++ H3_Objects/txt.md
@@ -0,0 +1,9 @@
+Maak een pagina met drie invoervakken en een knop.
+-Eén invoervak is voor rentepercentage (per jaar).
+-Eén invoervak is voor bedrag
+Controleer ook hier op mogelijke onjuistheden
+
+-button die saldo berekent per jaar
+met ingevoerde data in de uitput van de berekening
+en doorgaan tot het start bedrag is verdubbelt
+met rentepercentage
